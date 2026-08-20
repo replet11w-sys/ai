8a1d6964c55473fead07f50823a581f693a4ec96
replet11w-sys <replet11w@gmail.com>
2026-08-20 17:25:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// نقاط النهاية الخاصة بالمراقبة لإبقاء التطبيق نشطاً
 app.get('/', (req, res) => res.send('Status: OK'));
 app.get('/health', (req, res) => res.status(200).send('OK'));
 
@@ -11,5 +10,5 @@ app.listen(port, () => {
     console.log(`[SERVER] Health check running on port ${port}`);
 });
 
-// تحميل وتشغيل البوت من الملف الثاني
+// تحميل البوت
 require('./back.js');
